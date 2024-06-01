6fa148c38b343a4b93a2959243c46826526e98e3
Malena-Rigazio <rigaziomalena@gmail.com>
2024-05-31 21:03:37 -0300

    TP4
    Trabajo a medias

--- practicos/16 - 58874 - Rigazio, Malena Soledad/TP4/main.jsx
+++ practicos/16 - 58874 - Rigazio, Malena Soledad/TP4/main.jsx
@@ -1,7 +1,73 @@
+import React, { useState } from 'react';
+import React, {useEffect} from 'react';
 
-const API_KEY = '30d38b26954359266708f92e1317dac0'
+const API_KEY = '30d38b26954359266708f92e1317dac0';
+
 function App() {
-    return <>
-        <h1>App Clima</h1>
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        fetchWeatherData('Salta');
+    }, 
+    []);
+
+    const fetchWeatherData = async (city) => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=-24.7829&lon=-65.4122&appid=${API_KEY}`);
+            
+            if(!response.ok){
+                throw new Error('No se pudo obtener los datos del clima');
+            }
+
+            const data = await response.json();
+
+            setWeatherData(data);
+        } catch (error) {
+            console.error('Error al obtener datos del clima', error);
+        }
+    };
+
+    const handleCiudadClick = (city) => {
+        fetchWeatherData(city);
+    };
+return (
+    <>
+        <nav>
+            <ul>
+                <li>
+                    <h1>Clima</h1>
+                </li>
+            </ul>
+
+            <ul>
+                <li><a href="#" className="contrast" onClick={() => handleCiudadClick('Tucumán')}>Tucumán</a></li>
+                <li><a href="#" className="contrast" onClick={() => handleCiudadClick('Salta')}>Salta</a></li>
+                <li><a href="#" className="contrast" onClick={() => handleCiudadClick('Buenos Aires')}>Buenos Aires</a></li>
+            </ul>
+        </nav>
+
+        <input type="search" name="search" 
+        value={ciudadSeleccionada} placeholder="Buscar ciudad" 
+        aria-label="search" onChange={(e) => setCiudadSeleccionada(e.target.value)}/>
+        
+        {weatherData && (
+        <article>
+            <header>
+                <h2>{weatherData.name}</h2>
+            </header>
+            <img
+                src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                alt={weatherData.weather[0].description} height="48"/>
+            <footer>
+                <h3>Temperatura: {weatherData.main.temp}°C</h3>
+                <p className="m">Mínima: {weatherData.main.max}°C</p>
+                <p className="m">/ Máxima: {weatherData.main.min}°C</p>
+                <p className="hum">Humedad: {weatherData.main.humedity}%</p>
+            </footer>
+        </article>
+        )}
     </>
+    );
 }
+
+export default App;
